2cf50fb43302240e5ecb8c3e49a25d431c26ce8e
Noam Kleiner <noam.k@compie.co.il>
2019-05-06 17:26:15 +0300

    updated utilservice

--- frontend/src/services/util.service.js
+++ frontend/src/services/util.service.js
@@ -2,9 +2,6 @@
 
 export default {
     getRandomInt,
-    makeId,
-    makeLorem,
-    getCurrency
 }
 
 
@@ -12,60 +9,4 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-function makeId(length=3) {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  
-    for (let i = 0; i < length; i++)
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-  
-    return text;
-  }
-
-function getCurrency(currencyCode) {
-    switch(currencyCode){
-        case 'ILS' : 
-            return '₪';
-        case 'EUR' : 
-            return '€';
-        case 'USD' :
-            return '$';
-        default:
-            return '';
-    }
-}
-
-function makeLorem(length) {
-
-    let randStr = '';
-    while (randStr.length < length) {
-        const wordLength = getRandomInt(3, 6);
-        let word = _createWord(wordLength);
-
-        if (Math.random() > 0.9) word += ',';
-
-        randStr += word + ' ';
-    }
-    randStr = randStr.substring(0, length);
-    randStr = randStr[0].toUpperCase() + randStr.substr(1)
-
-    return randStr;
-}
-
-function _createWord(length) {
-    let word = '';
-    while (word.length < length) {
-        let randChar = _getRandChar();
-        word += randChar;
-    }
-
-    return word;
-}
-
-function _getRandChar() {
-    const LETTERS = 'abcdefghijklmnopqrstuvwxyz';
-    const randIndex = parseInt(Math.random() * LETTERS.length)
-    return LETTERS.charAt(randIndex);
 }
